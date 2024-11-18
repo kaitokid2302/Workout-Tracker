fc52ab950e634dee5392635e41a2c5a37a7f40a9
Đinh Trường Lãm <mra2322001@gmail.com>
2024-11-18 12:04:08 +0300

    tạo interface service và repository

---
+++ internal/repository/workout.go
@@ -0,0 +1,18 @@
+package repository
+
+import "github.com/kaitokid2302/Workout-Tracker/internal/db"
+
+type WorkoutRepository interface {
+	FindUserByUsername(username string) (*db.User, error)
+	CreateEmptyWorkout(user *db.User) error
+	AddExerciseToWorkout(workout *db.Workout, exercise *db.Exercise) error
+	DeleteExercise(workout *db.Workout, exerciseID uint) error
+	EditComment(workout *db.Workout, comment string) error
+	DeleteWorkout(workout *db.Workout) error
+	ScheduleWorkout(workout *db.Workout, time string) error
+	FinishWorkout(workout *db.Workout) error
+	WorkoutToday(user *db.User) ([]*db.Workout, error)
+	WorkoutFuture(user *db.User) ([]*db.Workout, error)
+	WorkoutDone(user *db.User) ([]*db.Workout, error)
+	PastWorkout(user *db.User) ([]*db.Workout, error)
+}
--- internal/service/workout.go
+++ internal/service/workout.go
@@ -1,12 +1,21 @@
 package service
 
 import (
+	"time"
+
 	"github.com/kaitokid2302/Workout-Tracker/internal/db"
 )
 
-type Workout interface {
+type WorkoutService interface {
 	CreateWorkout(user string, exercise []*db.Exercise) error
 	DeleteExercise(user string, exerciseID uint) error
 	AddExercise(user string, exercise *db.Exercise) error
 	EditComment(user string, comment string) error
+	DeleteWorkout(user string, exerciseID uint) error
+	ScheDuelWorkout(user uint, workoutID uint, time time.Time)
+	FinishWorkout(user uint, workoutID uint) error
+	WorkoutToday(user uint) []*db.Workout
+	WorkoutFuture(user uint) []*db.Workout
+	WorkoutDone(user uint) []*db.Workout
+	PastWorkout(user uint) []*db.Workout
 }
