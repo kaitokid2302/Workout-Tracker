06f5962990e4ec3ee6ff5a5f0c8fc9923005b5ba
Đinh Trường Lãm <mra2322001@gmail.com>
2024-11-19 19:28:24 +0300

    Bắt đầu implement WorkoutService

--- internal/service/workout.go
+++ internal/service/workout.go
@@ -19,3 +19,49 @@ type WorkoutService interface {
 	WorkoutDone(user uint) []*db.Workout
 	PastWorkout(user uint) []*db.Workout
 }
+
+type WorkoutServiceImpl struct{}
+
+func (w *WorkoutServiceImpl) CreateWorkout(user string, exercise []*db.Exercise) error {
+	panic("not implemented") // TODO: Implement
+}
+
+func (w *WorkoutServiceImpl) DeleteExercise(user string, exerciseID uint) error {
+	panic("not implemented") // TODO: Implement
+}
+
+func (w *WorkoutServiceImpl) AddExercise(user string, exercise *db.Exercise) error {
+	panic("not implemented") // TODO: Implement
+}
+
+func (w *WorkoutServiceImpl) EditComment(user string, comment string) error {
+	panic("not implemented") // TODO: Implement
+}
+
+func (w *WorkoutServiceImpl) DeleteWorkout(user string, exerciseID uint) error {
+	panic("not implemented") // TODO: Implement
+}
+
+func (w *WorkoutServiceImpl) ScheDuelWorkout(user uint, workoutID uint, time time.Time) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (w *WorkoutServiceImpl) FinishWorkout(user uint, workoutID uint) error {
+	panic("not implemented") // TODO: Implement
+}
+
+func (w *WorkoutServiceImpl) WorkoutToday(user uint) []*db.Workout {
+	panic("not implemented") // TODO: Implement
+}
+
+func (w *WorkoutServiceImpl) WorkoutFuture(user uint) []*db.Workout {
+	panic("not implemented") // TODO: Implement
+}
+
+func (w *WorkoutServiceImpl) WorkoutDone(user uint) []*db.Workout {
+	panic("not implemented") // TODO: Implement
+}
+
+func (w *WorkoutServiceImpl) PastWorkout(user uint) []*db.Workout {
+	panic("not implemented") // TODO: Implement
+}
